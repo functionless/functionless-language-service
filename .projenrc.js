56e32e73012b992ed9e154844758910a4dee17ae
sam <sam@functionless.org>
2022-06-15 19:22:48 -0700

    chore: add description for the NPM package

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,8 @@ const { typescript } = require("projen");
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: "main",
   name: "@functionless/language-service",
-
+  description:
+    "A TypeScript Language Service Plugin that shows Functionless errors in the IDE",
   peerDeps: ["functionless@^0.6.20"],
   devDeps: ["functionless"],
   eslintOptions: {
